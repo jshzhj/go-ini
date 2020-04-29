051493504f396666d98b3c82d42be197866b2050
zhj <abc1143356987@163.com>
2020-04-29 10:11:26 +0800

    增加README.md文件,完善注释

--- parse_test.go
+++ parse_test.go
@@ -5,14 +5,15 @@ import (
 )
 
 //节点
+// `ini:"server"`为配置文件节点到结构体字段的映射,可写可不写,不写默认为结构体字段名小写
 type Config struct {
-	ServerConf ServerConfig `ini:"server"`
-	Mysql      MysqlConfig
+	ServerConf ServerConfig `ini:"server"` //有映射(tag),则结构体字段名随意写
+	Mysql      MysqlConfig  //无映射(tag),则结构体字段名要与配置文件节点名对应
 }
 
 //选项
 type ServerConfig struct {
-	Ipaa string `default:"192.168.1.1" ini:"ip"`
+	Ip   string `default:"192.168.1.1" ini:"ip"` //default:默认值(可有可无) ini:映射(同上,可有可无)
 	Port uint   `default:"8080"`
 }
 
@@ -24,7 +25,7 @@ type MysqlConfig struct {
 
 func TestConfigToStruct(t *testing.T) {
 	iniPath := "config.ini" //配置文件路径
-	conf := &Config{}
+	conf := &Config{}       //实力化结构体
 
 	err := Parse(iniPath, conf)
 	if err != nil {
